382e7211ab09f03eec609f19eb2a295253e8cc66
Rushikesh Mind <157093618+RusheeMind55@users.noreply.github.com>
2024-03-30 11:14:06 +0530

    Add files via upload

---
+++ ArrayList1.java
@@ -0,0 +1,24 @@
+package Collection;
+
+public class ArrayList1 {
+	
+		int Id;
+		int RollNo;
+		String Name;
+		
+		public ArrayList1(int id, int rollNo, String name) {	super();
+		Id = id;
+		RollNo = rollNo;
+			Name = name;
+		}
+
+	
+		
+		@Override
+	public String toString() {
+			return "ArrayList1 [Id=" + Id + ", RollNo=" + RollNo + ", Name=" + Name + "]";
+		}
+	
+	
+
+}
