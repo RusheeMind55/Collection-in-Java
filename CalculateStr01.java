d1325df9187f3b49bd5b7f2234bf491ed3c7bf86
Rushikesh Mind <157093618+RusheeMind55@users.noreply.github.com>
2024-04-04 08:21:00 +0530

    Add files via upload

---
+++ CalculateStr01.java
@@ -0,0 +1,19 @@
+package ArrayInterviewQue;
+
+public class CalculateStr01 {
+
+	public static void main(String[] args) {
+
+		String str="FACEBOOK";
+		int calculate=0;
+		for(int i=0;i<str.length();i++) {
+			
+			if(str.charAt(i) != ' ') 
+				calculate++;
+				System.out.println(str.charAt(i)+""+calculate);
+				
+			}
+		}
+	}
+
+
